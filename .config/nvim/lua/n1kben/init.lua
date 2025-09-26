8bfef94ebc4a3302755eb82ce7565c4b715b705a
Nik Ben <github.n1k62dgy@3post.se>
2025-09-26 08:02:14 +0200

    tmp

--- .config/nvim/lua/n1kben/init.lua
+++ .config/nvim/lua/n1kben/init.lua
@@ -5,3 +5,10 @@ require("n1kben.lsp")
 require("n1kben.formatters")
 require("n1kben.regex-popup").setup()
 require("n1kben.todo")
+require("n1kben.alternate").setup({
+  dual_patterns = {
+    { "*.res",  "*_sandbox.res", "Source File",     "Sandbox File" },
+    { "*.res",  "*.bs.mjs",      "ReScript Source", "Compiled JS" },
+    { "*.resi", "*.res",         "Interface File",  "Implementation File" },
+  }
+})
