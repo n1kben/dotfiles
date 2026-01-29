070fa4fce270f223480786e3a4f6e9cba9c446ad
Nik Ben <github.n1k62dgy@3post.se>
2026-01-29 11:50:16 +0100

    🔧 chore: disable askToStartBuild in rescript LSP

--- .config/nvim/lsp/rescript.lua
+++ .config/nvim/lsp/rescript.lua
@@ -2,4 +2,9 @@ return {
   cmd = { "rescript-language-server", "--stdio" },
   filetypes = { "rescript" },
   root_markers = { ".rescript.json", ".bsconfig.json" },
+  init_options = {
+    settings = {
+      askToStartBuild = false,
+    },
+  },
 }
