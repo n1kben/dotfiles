f7f90484dee24eb04c3aee4a53e62e6498c784e3
Nik Ben <github.n1k62dgy@3post.se>
2026-02-05 21:21:11 +0100

    Skip git commit filetypes in lastplace
    Ignore gitcommit, gitrebase, and special buftypes so the cursor
    doesn't jump to the last position in commit message buffers.

--- .config/nvim/lua/n1kben/lastplace.lua
+++ .config/nvim/lua/n1kben/lastplace.lua
@@ -2,8 +2,17 @@ local M = {}
 
 -- Setup
 function M.setup()
+  local ignore_filetypes = { "gitcommit", "gitrebase", "commit" }
+
   vim.api.nvim_create_autocmd("BufReadPost", {
     callback = function()
+      if vim.bo.buftype ~= "" then return end
+
+      local ft = vim.bo.filetype
+      for _, ignored in ipairs(ignore_filetypes) do
+        if ft == ignored then return end
+      end
+
       local mark = vim.api.nvim_buf_get_mark(0, '"')
       local lcount = vim.api.nvim_buf_line_count(0)
       if mark[1] > 0 and mark[1] <= lcount then
