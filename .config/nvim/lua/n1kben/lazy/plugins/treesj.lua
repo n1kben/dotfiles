295005bef995f79b26bdaa73a25b12362ad31198
Nik Ben <github.n1k62dgy@3post.se>
2026-02-05 21:35:21 +0100

    Add quicker.nvim plugin, add treesj fallback keymaps
    Add quicker.nvim for enhanced quickfix list with expandable context.
    Make treesj gj/gk fall back to native join/split when no treesitter
    parser is available.

--- .config/nvim/lua/n1kben/lazy/plugins/treesj.lua
+++ .config/nvim/lua/n1kben/lazy/plugins/treesj.lua
@@ -1,9 +1,34 @@
+local function has_ts_parser()
+  local ok, parser = pcall(vim.treesitter.get_parser)
+  return ok and parser ~= nil
+end
+
 return {
   "Wansmer/treesj",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   keys = {
-    { "gj", function() require("treesj").join() end,  desc = "Join lines" },
-    { "gk", function() require("treesj").split() end, desc = "Split lines" },
+    {
+      "gj",
+      function()
+        if has_ts_parser() then
+          require("treesj").join()
+        else
+          vim.cmd("normal! J")
+        end
+      end,
+      desc = "Join lines",
+    },
+    {
+      "gk",
+      function()
+        if has_ts_parser() then
+          require("treesj").split()
+        else
+          vim.cmd([[execute "normal! i\<CR>"]])
+        end
+      end,
+      desc = "Split lines",
+    },
   },
   opts = {
     use_default_keymaps = false,
