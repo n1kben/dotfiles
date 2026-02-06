47b4107c545938203522686f26427f69d895f7dc
Nik Ben <github.n1k62dgy@3post.se>
2026-02-06 13:19:53 +0100

    Enhance statusline: Oil paths, colored branch, smart truncation

--- .config/nvim/lua/n1kben/statusline.lua
+++ .config/nvim/lua/n1kben/statusline.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 function M.setup()
-  local branch = ""
+  local branch_name = ""
+  local branch_icon = " "
 
   local function update_branch()
     local b = vim.fn.system("git branch --show-current 2>/dev/null"):gsub("\n", "")
-    branch = b ~= "" and " " .. b or ""
+    branch_name = b
   end
 
   update_branch()
@@ -14,10 +15,52 @@ function M.setup()
     callback = update_branch,
   })
 
+  vim.api.nvim_set_hl(0, "StatusBranch", { link = "Function" })
+
   vim.o.statusline = "%!v:lua.Statusline()"
 
+  local function short_path(path)
+    local home = vim.env.HOME
+    if home and path:sub(1, #home) == home then
+      path = "~" .. path:sub(#home + 1)
+    end
+    return path
+  end
+
   function Statusline()
-    return " %f %m %= " .. branch .. "  %l:%c "
+    local name = vim.api.nvim_buf_get_name(0)
+    local path, mod
+    if name:find("^oil://") then
+      path = short_path(name:sub(7))
+      mod = ""
+    elseif name ~= "" then
+      path = short_path(name)
+      mod = " %m"
+    else
+      path = "[No Name]"
+      mod = " %m"
+    end
+
+    local br = ""
+    if branch_name ~= "" then
+      local win_w = vim.api.nvim_win_get_width(0)
+      local path_w = vim.fn.strdisplaywidth(path)
+      local icon_w = vim.fn.strdisplaywidth(branch_icon)
+      -- estimate fixed parts: spaces, %m, %l:%c, separators
+      local avail = win_w - path_w - icon_w - 15
+      local bname = branch_name
+      local min_len = math.min(10, #bname)
+      if avail < #bname then
+        local len = math.max(min_len, avail)
+        bname = bname:sub(1, len)
+        if len < #branch_name then
+          bname = bname .. "…"
+        end
+      end
+      br = "%#StatusBranch#" .. branch_icon .. bname .. "%*"
+    end
+
+    return " %<" .. path .. mod .. " %= " .. br .. "  %l:%c "
   end
 end
 
